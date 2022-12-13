ba675fb52cee017d27cb0d867732c354c5ab96e6
piercenovo <piercenovoah@gmail.com>
2022-12-13 17:53:08 -0500

    update game button to guest

--- lib/src/features/games/presentation/views/game_detail/widgets/game_detail_start.dart
+++ lib/src/features/games/presentation/views/game_detail/widgets/game_detail_start.dart
@@ -1,8 +1,12 @@
+import 'package:brainconcent_flutter/src/core/helpers/animation_route.dart';
 import 'package:brainconcent_flutter/src/core/utils/colors.dart';
 import 'package:brainconcent_flutter/src/core/utils/size_config.dart';
 import 'package:brainconcent_flutter/src/core/widgets/primary_button.dart';
+import 'package:brainconcent_flutter/src/features/authentication/presentation/cubit/user/user_cubit.dart';
+import 'package:brainconcent_flutter/src/features/authentication/presentation/views/login/login_screen.dart';
 import 'package:brainconcent_flutter/src/features/games/domain/entities/game/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class GameDetailStart extends StatelessWidget {
   const GameDetailStart({
@@ -17,20 +21,40 @@ class GameDetailStart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      child: PrimaryButton(
-        height: getProportionateScreenHeight(58),
-        width: getProportionateScreenWidth(310),
-        text: 'Empezar',
-        bgColor: kSecondaryColorTwo,
-        textColor: kLightColor,
-        onPressed: () {
-          Navigator.pushNamed(context, game.link, arguments: {
-            'uid': game.uid,
-          });
-        },
-        fontSize: width * 4.1,
-      ),
-    );
+        alignment: Alignment.center,
+        child: BlocBuilder<UserCubit, UserState>(
+          builder: (_, state) => (state.user == null)
+              ? PrimaryButton(
+                  height: getProportionateScreenHeight(58),
+                  width: getProportionateScreenWidth(310),
+                  text: 'Empezar',
+                  bgColor: kSecondaryColorTwo,
+                  textColor: kLightColor,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      routeGo(page: const LoginScreen()),
+                    );
+                  },
+                  fontSize: width * 4.1,
+                )
+              : PrimaryButton(
+                  height: getProportionateScreenHeight(58),
+                  width: getProportionateScreenWidth(310),
+                  text: 'Empezar',
+                  bgColor: kSecondaryColorTwo,
+                  textColor: kLightColor,
+                  onPressed: () {
+                    Navigator.pushNamed(
+                      context,
+                      game.link,
+                      arguments: {
+                        'uid': game.uid,
+                      },
+                    );
+                  },
+                  fontSize: width * 4.1,
+                ),
+        ));
   }
 }
